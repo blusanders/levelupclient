1c8ef325def17e20192973e39984f567a33287f7
blusanders <blusanders@yahoo.com>
2021-05-14 15:23:36 -0500

    added game count

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -28,6 +28,7 @@ export const GameList = (props) => {
                         <div className="game__title">{game.title} by {game.maker}</div>
                         <div className="game__players">{game.number_of_players} players needed</div>
                         <div className="game__skillLevel">Skill level is {game.skill_level}</div>
+                        <div className="game__eventCount">Count {game.event_count}</div>
                         <div>
                             <button className="btn btn-1 btn-sep icon-create"
                             onClick={() => {
